24b9dae59970ebae6f75193efb64bca9985259b1
ChaSang-geol <56263370+ChaSang-geol@users.noreply.github.com>
2020-05-18 11:09:24 +0900

    Update test.java

--- test.java
+++ test.java
@@ -1,2 +1,17 @@
-test
-tetst 
+mkdir git
+cd git
+git init
+git config --global user.name ChaSang-geol
+git config --global user.email hunipapa@nate.com
+
+echo test > test.java
+git add test.java
+git status
+
+git commit -m "first commit"
+
+git remote add origin https://github.com/ChaSang-geol/edu-repository.git
+git push -u origin master
+
+git pull
+git log
